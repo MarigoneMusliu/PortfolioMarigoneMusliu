4d7044f5d3f082d38df194ee247577430e193033
MarigoneMusliu <109240085+MarigoneMusliu@users.noreply.github.com>
2024-12-01 23:03:00 +0100

    first commit

---
+++ utils/data/personal-data.js
@@ -0,0 +1,21 @@
+export const personalData = {
+  name: "MARIGONË MUSLIU",
+  profile: "/profile.png",
+  designation: "Software Developer",
+  description:
+    "My name is Marigonë Musliu, and I’m a programmer who loves learning and exploring new ideas. I enjoy solving problems and working on projects that challenge me to grow and improve. I’m passionate about what I do and always try to give my best in everything.I’m motivated, hardworking, and open to new opportunities where I can contribute, learn, and make a difference.My primary expertise lies in JavaScript, React, and SQL, and I enjoy working across various web development stacks to create applications that are functional, user-friendly, and accessible.",
+  email: "marigonamusliu48@gmail.com",
+  phone: "+38349733741",
+  address: "Gjilan ,Kosovo",
+  github: "https://github.com/marigonemusliu",
+  facebook: "https://www.facebook.com/marigona.musliu.90/",
+  linkedIn: "https://www.linkedin.com/in/marigon%C3%AB-musliu-a4a3b1245/",
+  twitter: "https://x.com/MarigoneM",
+  stackOverflow:
+    "https://stackoverflow.com/users/18111741/marigon%c3%ab-musliu",
+  devUsername: "marigonemusliu",
+  leetcode: "https://leetcode.com/u/MarigonaMusliu/",
+  devUsername: "marigonemusliu",
+  resume:
+    "https://drive.google.com/file/d/1AkFnbZ4hBXXDqNAk5L3kg2DgF6-ogCpV/view?usp=sharing",
+};
---
+++ utils/data/projects-data.js
@@ -0,0 +1,54 @@
+import ayla from "/public/image/ayla.jpg";
+import crefin from "/public/image/crefin.jpg";
+import realEstate from "/public/image/real-estate.jpg";
+import travel from "/public/image/travel.jpg";
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Quiz Game App",
+    tools: ["HTML", "CSS", "JavaScript", "SQL", "MySQL", "PHP"],
+    myRole: "Backend Developer",
+    description:
+      "My team and I built a quiz game app where users can test their programming language knowledge. I used two modules—UserModule and AdministrationModule—to structure the application. I worked on the backend, utilizing SQL and PHP to manage the database for questions and user scores. The app dynamically updates based on user input, making it an engaging and interactive platform for testing programming skills.",
+    githubLink: "https://github.com/MarigoneMusliu/QuizGame",
+    demoLink:
+      "https://https-github-com-marigone-musliu-quiz-game-app-photos.vercel.app/", // Custom demo link for Quiz Game
+    image: crefin,
+  },
+  {
+    id: 2,
+    name: "Skin Care Mari",
+    tools: ["HTML", "CSS", "JavaScript", "PHP", "SQL", "MySQL", "JSON"],
+    myRole: "Full Stack Developer",
+    description:
+      "This project is a web-based system for managing a skincare business. I developed two modules—UserModule for customer interactions and AdminModule for managing products, users, and orders. The backend was built with PHP and MySQL for secure database operations, and the frontend is designed to be intuitive and user-friendly.",
+    githubLink: "https://github.com/MarigoneMusliu/SkinCareMari",
+    demoLink:
+      "https://https-github-com-marigone-musliu-skin-care-mari-project-photo.vercel.app/", // Custom demo link for Skin Care Mari
+    image: travel,
+  },
+  {
+    id: 3,
+    name: "Hotel Reservation Management",
+    tools: ["HTML", "CSS", "JavaScript", "Laravel"],
+    myRole: "Full Stack Developer",
+    description:
+      "I developed a hotel reservation system to simplify the booking process for customers and hotel staff. Users can reserve rooms, while staff can view, update, and manage all reservations. The system tracks key information, such as room types and booking dates, with a smooth and responsive interface built using Laravel and JavaScript.",
+    githubLink: "https://github.com/MarigoneMusliu/Hotel-Reservation-",
+    demoLink:
+      "https://https-github-com-marigone-musliu-hotel-management-reservation.vercel.app/", // Custom demo link for Hotel Reservation
+    image: realEstate,
+  },
+  {
+    id: 4,
+    name: "Movie App",
+    tools: ["HTML", "CSS", "JavaScript", "ReactJS"],
+    myRole: "ReactJS Developer",
+    description:
+      "I created a movie app that organizes TV shows and movies into categories for better navigation. The app was built with ReactJS, featuring a modern design and smooth user experience. It allows users to browse and explore entertainment options efficiently.",
+    githubLink: "https://github.com/MarigoneMusliu/movie-app2",
+    demoLink: "https://movie-app2-eight.vercel.app/", // Custom demo link for Movie App
+    image: ayla,
+  },
+];
