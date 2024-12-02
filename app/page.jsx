78c447656eb83573321bd4593daecf3a6bfba8b3
rinormaliqi <rmaliqi75@gmail.com>
2024-12-02 14:52:49 +0100

    someFixes7

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomePageComponent from './MainPageComponent'
+
+const HomePage = () => {
+  return (
+    <>
+    <HomePageComponent/>
+    </>
+  )
+}
+
+export default HomePage
