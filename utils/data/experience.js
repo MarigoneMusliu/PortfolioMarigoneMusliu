32b7b42c04c10c6e6821c3ee8d8dd4942574fdcf
Rinori <141419732+rinormaliqi@users.noreply.github.com>
2024-12-05 15:09:50 +0100

    Update experience.js
    remove tecagon

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,24 +1,18 @@
 export const experiences = [
-  {
+ {
     id: 1,
-    title: "Software Developer Internship",
-    company: "Tectigon",
-    duration: "(Dec 2024 - Present)",
-  },
-  {
-    id: 2,
     title: "Junior on Advanced JavaScript & ReactJs Course",
     company: "Beetroot Academy",
     duration: "(Dec 2023 - Feb 2024)",
   },
   {
-    id: 3,
+    id: 2,
     title: "Applied Science Course",
     company: "Austrian Development Cooperation",
     duration: "(Jan 2023)",
   },
   {
-    id: 4,
+    id: 3,
     title: "Python",
     company: "Roi Academy",
     duration: "(Apr 2022)",
