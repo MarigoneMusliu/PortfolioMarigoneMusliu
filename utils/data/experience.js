013893add30a2be9ba4231e4fade91c92e58b56b
Rinori <141419732+rinormaliqi@users.noreply.github.com>
2024-12-05 15:18:14 +0100

    Update experience.js
    as

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,18 +1,24 @@
 export const experiences = [
- {
+   {
     id: 1,
+    title: "Full Stack Developer",
+    company: "Tectigon",
+    duration: "(Oct 2024 - Present)",
+  },
+ {
+    id: 2,
     title: "Junior on Advanced JavaScript & ReactJs Course",
     company: "Beetroot Academy",
     duration: "(Dec 2023 - Feb 2024)",
   },
   {
-    id: 2,
+    id: 3,
     title: "Applied Science Course",
     company: "Austrian Development Cooperation",
     duration: "(Jan 2023)",
   },
   {
-    id: 3,
+    id: 4,
     title: "Python",
     company: "Roi Academy",
     duration: "(Apr 2022)",
