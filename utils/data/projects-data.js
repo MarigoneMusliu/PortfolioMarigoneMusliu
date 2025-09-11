c0df123adcd3634d1f57dca3cc4c018c51b128b5
marigonemusliu <marigonamusliu48@gmail.com>
2025-09-11 21:17:02 +0200

    Merged with remote repository

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -6,7 +6,6 @@ import travel from "/public/image/travel.jpg";
 export const projectsData = [
   {
     id: 1,
-<<<<<<< HEAD
     name: "Private Business Projects",
     tools: [""],
     myRole: "",
@@ -47,74 +46,5 @@ export const projectsData = [
       "Developed a personal e-commerce website for electric bikes using React.js, focusing on interactive UI, responsive design, and modern web development practices. This project showcases my skills in front-end development and user interface design.",
     githubLink: "https://github.com/MarigoneMusliu/E-Commerce-E-Bike",
     demoLink: "https://e-commerce-e-bike.vercel.app/", // Custom demo link for Movie App
-=======
-    name: "Real Estate",
-    tools: ["HTML", "CSS", "JavaScript"],
-    myRole: "FrontEnd  Developer",
-    description:
-      "I developed a real estate website on my own using HTML, CSS, and JavaScript, where I showcased properties for sale or rent. The platform allows users to view property listings with details such as room descriptions, pricing, and location. As part of the project, I created a promotional section for a real estate agency in Kosovo, highlighting their services and available properties. The website is designed to provide potential clients with easy navigation and an informative experience when searching for properties.",
-    githubLink: "https://github.com/MarigoneMusliu/RealEstate",
-    demoLink: "https://real-estate-mu-rosy.vercel.app/", // Custom demo link for Quiz Game
-    image: crefin,
-  },
-  {
-    id: 1,
-    name: "Bycicle E-Commerce ",
-    tools: ["HTML", "CSS", "JavaScript", "Vue.js"],
-    myRole: "Full Stack Developer",
-    description:
-      "I developed an e-commerce platform for purchasing electric bikes on my own, using HTML, CSS, JavaScript, and Vue.js. The website allows users to browse different bike models, add items to their cart, and manage their shopping experience. I also implemented features such as adding bikes to a favorites list, removing items from the cart, and a contact section for customer inquiries. The dynamic nature of the site is powered by Vue.js, providing an interactive and smooth user experience throughout the purchasing process.",
-    githubLink: "https://github.com/MarigoneMusliu/E-Commerce-E-Bike",
-    demoLink: "https://e-commerce-e-bike.vercel.app/", // Custom demo link for Quiz Game
-    image: crefin,
-  },
-  {
-    id: 3,
-    name: "Quiz Game App",
-    tools: ["HTML", "CSS", "JavaScript", "SQL", "MySQL", "PHP"],
-    myRole: "Backend Developer",
-    description:
-      "My team and I built a quiz game app where users can test their programming language knowledge. I used two modules—UserModule and AdministrationModule—to structure the application. I worked on the backend, utilizing SQL and PHP to manage the database for questions and user scores. The app dynamically updates based on user input, making it an engaging and interactive platform for testing programming skills.",
-    githubLink: "https://github.com/MarigoneMusliu/QuizGame",
-    demoLink:
-      "https://https-github-com-marigone-musliu-quiz-game-app-photos.vercel.app/", // Custom demo link for Quiz Game
-    image: crefin,
-  },
-
-  {
-    id: 4,
-    name: "Skin Care Mari",
-    tools: ["HTML", "CSS", "JavaScript", "PHP", "SQL", "MySQL", "JSON"],
-    myRole: "Full Stack Developer",
-    description:
-      "This project is a web-based system for managing a skincare business. I developed two modules: the User Module for customer interactions and the Admin Module for managing products, users, and orders. The backend system was built using PHP and MySQL for secure database operations, while the frontend is intuitive and user-friendly.",
-    githubLink: "https://github.com/MarigoneMusliu/SkinCareMari",
-    demoLink:
-      "https://https-github-com-marigone-musliu-skin-care-mari-project-photo.vercel.app/", // Custom demo link for Skin Care Mari
-    image: travel,
-  },
-  {
-    id: 5,
-    name: "Hotel Reservation Management",
-    tools: ["HTML", "CSS", "JavaScript", "Laravel"],
-    myRole: "Full Stack Developer",
-    description:
-      "I developed a hotel reservation system to simplify the booking process for customers and hotel staff. Users can reserve rooms, while staff can view, update, and manage all reservations. The system tracks key information, such as room types and booking dates, with a smooth and responsive interface built using Laravel and JavaScript.",
-    githubLink: "https://github.com/MarigoneMusliu/Hotel-Reservation-",
-    demoLink:
-      "https://https-github-com-marigone-musliu-hotel-management-reservation.vercel.app/", // Custom demo link for Hotel Reservation
-    image: realEstate,
-  },
-  {
-    id: 6,
-    name: "Movie App",
-    tools: ["HTML", "CSS", "JavaScript", "ReactJS"],
-    myRole: "ReactJS Developer",
-    description:
-      "I created a movie app that organizes TV shows and movies into categories for better navigation. The app was built with ReactJS, featuring a modern design and smooth user experience. It allows users to browse and explore entertainment options efficiently.",
-    githubLink: "https://github.com/MarigoneMusliu/movie-app2",
-    demoLink: "https://movie-app2-eight.vercel.app/", // Custom demo link for Movie App
->>>>>>> 78cd480026a94336ed4b50724fcdfacfc3457ef5
-    image: ayla,
-  },
+  }
 ];
