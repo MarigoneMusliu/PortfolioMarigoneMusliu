67a7594ed8fa3dbc0dd7cebb3abc2c4914622dee
marigonemusliu <marigonamusliu48@gmail.com>
2025-09-11 21:46:10 +0200

    Merged with remote repository

--- app/components/homepage/experience/index.jsx
+++ app/components/homepage/experience/index.jsx
@@ -5,22 +5,11 @@ import Image from "next/image";
 import { BsPersonWorkspace } from "react-icons/bs";
 import AnimationLottie from "../../helper/animation-lottie";
 import GlowCard from "../../helper/glow-card";
-<<<<<<< HEAD
 import experience from '/public/lottie/code.json';
 
 function Experience() {
   return (
     <div id="experience" className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
-=======
-import experience from "/public/lottie/code.json";
-
-function Experience() {
-  return (
-    <div
-      id="experience"
-      className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]"
-    >
->>>>>>> 78cd480026a94336ed4b50724fcdfacfc3457ef5
       <Image
         src="/section.svg"
         alt="Hero"
@@ -30,11 +19,7 @@ function Experience() {
       />
 
       <div className="flex justify-center my-5 lg:py-8">
-<<<<<<< HEAD
         <div className="flex  items-center">
-=======
-        <div className="flex items-center">
->>>>>>> 78cd480026a94336ed4b50724fcdfacfc3457ef5
           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
           <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-xl rounded-md">
             Experiences
@@ -53,7 +38,6 @@ function Experience() {
 
           <div>
             <div className="flex flex-col gap-6">
-<<<<<<< HEAD
               {
                 experiences.map(experience => (
                   <GlowCard key={experience.id} identifier={`experience-${experience.id}`}>
@@ -80,68 +64,19 @@ function Experience() {
                           </p>
                           <p className="text-sm sm:text-base">
                             {experience.company}
-=======
-              {experiences.map((experience) => (
-                <GlowCard
-                  key={experience.id}
-                  identifier={`experience-${experience.id}`}
-                >
-                  <div className="p-3 relative">
-                    <Image
-                      src="/blur-23.svg"
-                      alt="Hero"
-                      width={1080}
-                      height={200}
-                      className="absolute bottom-0 opacity-80"
-                    />
-                    <div className="flex justify-center">
-                      <p className="text-xs sm:text-sm text-[#16f2b3]">
-                        {experience.duration}
-                      </p>
-                    </div>
-                    <div className="flex items-center gap-x-8 px-3 py-5">
-                      <div className="text-violet-500  transition-all duration-300 hover:scale-125">
-                        <BsPersonWorkspace size={36} />
-                      </div>
-                      <div>
-                        <p className="text-base sm:text-xl mb-2 font-medium uppercase">
-                          {experience.title}
-                        </p>
-                        <p className="text-sm sm:text-base">
-                          {experience.company}
-                        </p>
-                        <div className="mt-3">
-                          <p className="text-sm sm:text-base font-medium">
-                            Duties:
-                          </p>
-                          <p className="text-sm sm:text-base">
-                            {experience.duties}
->>>>>>> 78cd480026a94336ed4b50724fcdfacfc3457ef5
                           </p>
                         </div>
                       </div>
                     </div>
-<<<<<<< HEAD
                   </GlowCard>
                 ))
               }
-=======
-                  </div>
-                </GlowCard>
-              ))}
->>>>>>> 78cd480026a94336ed4b50724fcdfacfc3457ef5
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-<<<<<<< HEAD
 };
 
 export default Experience;
-=======
-}
-
-export default Experience;
->>>>>>> 78cd480026a94336ed4b50724fcdfacfc3457ef5
