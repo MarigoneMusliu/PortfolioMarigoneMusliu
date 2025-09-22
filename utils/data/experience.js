c827ab100ebf1ff243234f7bc8f157c78d7ebc67
marigonemusliu <marigonamusliu48@gmail.com>
2025-09-23 01:10:51 +0200

    Update personal-data.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,23 +1,15 @@
 export const experiences = [
-    {
+  {
     id: 1,
-    title:
-      "Junior Bicycle Designer & Digital Creator | Social Media, IT & Mechatronics",
+    title: "IT & Graphic Designer",
     company: "Albimi – Manufacturer of Pilot Bicycles",
     duration: "Feb 2025 – Present",
     duties:
-      "Manage and maintain the company's website, ensuring it is up-to-date and functional. Oversee the internal network infrastructure, providing support for system security, and troubleshooting IT-related issues. Assist in the configuration and optimization of IT systems to improve company operations. In addition, work on basic mechatronics tasks, programming robots to handle bicycle type changes. Design bicycles and manage social media platforms, creating engaging content and visual materials for online presence."
+      "Designed bicycle models and created 3D templates and visuals for production,Provided IT support, including network maintenance, troubleshooting, and data management.Managed and optimized the company’s social media presence, creating engaging content and marketing materials.Supported printing and technical design processes for product development".
   },
+
   {
     id: 2,
-    title: "Python Training",
-    company:
-      "EU Support for the Competitiveness of Kosovo’s ICT Sector Project”",
-    duration: "Feb 2025 - Present",
-    duties: "Python Training",
-  },
-  {
-    id: 3,
     title: "Full Stack Developer Training",
     company: "Tectigon Academy",
     duration: "(Oct 2024 - Dec 2024)",
@@ -25,7 +17,7 @@ export const experiences = [
       "Successfully completed a Full-Stack Developer training, covering PHP, SQL, JavaScript, ReactJS, HTML, and CSS . The program included one month of hands-on practice, applying learned technologies in real-world projects.",
   },
   {
-    id: 4,
+    id: 3,
     title: "Junior on Advanced JavaScript & ReactJs Training",
     company: "Beetroot Academy",
     duration: "(Dec 2023 - Feb 2024)",
@@ -33,19 +25,20 @@ export const experiences = [
       "In the Advanced JavaScript & ReactJs Course , I enhanced my skills in JavaScript and ReactJs, focusing on hooks, context API, state management, and routing to build dynamic, responsive web applications.",
   },
   {
-    id: 5,
+    id: 4,
     title: "Applied Science One Day Training",
     company: "Austrian Development Cooperation",
     duration: "(Jan 2023)",
-    duties:"Concrete Applications of Data Structures and Algorithms in Web and Mobile Development",
+    duties:
+      "Concrete Applications of Data Structures and Algorithms in Web and Mobile Development",
   },
   {
-    id: 6,
+    id: 5,
     title: "Python Training",
     company: "Roi Academy",
     duration: "(Apr 2022)",
     duties:
-      "I started a Python course and, after completing it, was offered a 1 month internship by the academy to further develop my skills in Python."
+      "I started a Python course and, after completing it, was offered a 1 month internship by the academy to further develop my skills in Python.",
   },
 ];
 
