c18912aa49fc32552822a7b7a5d364fdf3682d7d
marigonemusliu <marigonamusliu48@gmail.com>
2025-09-23 01:39:01 +0200

    Update personal-data.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -5,40 +5,42 @@ export const experiences = [
     company: "Albimi – Manufacturer of Pilot Bicycles",
     duration: "Feb 2025 – Present",
     duties:
-      "Designed bicycle models and created 3D templates and visuals for production,Provided IT support, including network maintenance, troubleshooting, and data management.Managed and optimized the company’s social media presence, creating engaging content and marketing materials.Supported printing and technical design processes for product development".
+      "Designed bicycle models and created 3D templates and visuals for production, provided IT support, including network maintenance, troubleshooting, and data management, managed and optimized the company’s social media presence, creating engaging content and marketing materials, supported printing and technical design processes for product development",
   },
 
   {
     id: 2,
     title: "Full Stack Developer Training",
     company: "Tectigon Academy",
-    duration: "(Oct 2024 - Dec 2024)",
+    duration: "Oct 2024 - Dec 2024",
     duties:
-      "Successfully completed a Full-Stack Developer training, covering PHP, SQL, JavaScript, ReactJS, HTML, and CSS . The program included one month of hands-on practice, applying learned technologies in real-world projects.",
+      "Successfully completed a Full-Stack Developer training, covering PHP, SQL, JavaScript, ReactJS, HTML, and CSS. The program included one month of hands-on practice, applying learned technologies in real-world projects.",
   },
+
   {
     id: 3,
     title: "Junior on Advanced JavaScript & ReactJs Training",
     company: "Beetroot Academy",
-    duration: "(Dec 2023 - Feb 2024)",
+    duration: "Dec 2023 - Feb 2024",
     duties:
-      "In the Advanced JavaScript & ReactJs Course , I enhanced my skills in JavaScript and ReactJs, focusing on hooks, context API, state management, and routing to build dynamic, responsive web applications.",
+      "In the Advanced JavaScript & ReactJs Course, I enhanced my skills in JavaScript and ReactJs, focusing on hooks, context API, state management, and routing to build dynamic, responsive web applications.",
   },
+
   {
     id: 4,
     title: "Applied Science One Day Training",
     company: "Austrian Development Cooperation",
-    duration: "(Jan 2023)",
+    duration: "Jan 2023",
     duties:
       "Concrete Applications of Data Structures and Algorithms in Web and Mobile Development",
   },
+
   {
     id: 5,
     title: "Python Training",
     company: "Roi Academy",
-    duration: "(Apr 2022)",
+    duration: "Apr 2022",
     duties:
-      "I started a Python course and, after completing it, was offered a 1 month internship by the academy to further develop my skills in Python.",
+      "I started a Python course and, after completing it, was offered a 1-month internship by the academy to further develop my skills in Python",
   },
 ];
-
