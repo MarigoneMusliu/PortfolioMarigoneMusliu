49ebccb97796199a5efa44c9068f3c468be6eb8d
marigonemusliu <marigone.musliu@behamics.com>
2025-12-20 15:41:37 +0100

    Update portfolio

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,15 +1,24 @@
 export const experiences = [
   {
     id: 1,
-    title: "IT & Graphic Designer",
-    company: "Albimi – Manufacturer of Pilot Bicycles",
-    duration: "Feb 2025 – Present",
+    title: "Front End Developer - Angular",
+    company: "Behamics Company",
+    duration: "Nov 2025 – Present",
     duties:
-      "Designed bicycle models and created 3D templates and visuals for production, provided IT support, including network maintenance, troubleshooting, and data management, managed and optimized the company’s social media presence, creating engaging content and marketing materials, supported printing and technical design processes for product development",
+      "Full-time engagement as a Front-end Developer in developing digital applications by creating modern user interfaces and collaborating with the development team on local and international projects in accordance with professional standards.",
   },
 
   {
     id: 2,
+    title: "IT & Graphic Designer",
+    company: "Albimi – Manufacturer of Pilot Bicycles",
+    duration: "Feb 2025 – Present",
+    duties:
+      "Designed bicycle models and created 3D templates and visuals for production, provided IT support, including network maintenance, troubleshooting, and data management, managed and optimized the company’s social media presence, creating engaging content and marketing materials, supported printing and technical design processes for product development.",
+  },
+
+  {
+    id: 3,
     title: "Full Stack Developer Training",
     company: "Tectigon Academy",
     duration: "Oct 2024 - Dec 2024",
@@ -18,7 +27,7 @@ export const experiences = [
   },
 
   {
-    id: 3,
+    id: 4,
     title: "Junior on Advanced JavaScript & ReactJs Training",
     company: "Beetroot Academy",
     duration: "Dec 2023 - Feb 2024",
@@ -27,20 +36,20 @@ export const experiences = [
   },
 
   {
-    id: 4,
+    id: 5,
     title: "Applied Science One Day Training",
     company: "Austrian Development Cooperation",
     duration: "Jan 2023",
     duties:
-      "Concrete Applications of Data Structures and Algorithms in Web and Mobile Development",
+      "Concrete Applications of Data Structures and Algorithms in Web and Mobile Development.",
   },
 
   {
-    id: 5,
+    id: 6,
     title: "Python Training",
     company: "Roi Academy",
     duration: "Apr 2022",
     duties:
-      "I started a Python course and, after completing it, was offered a 1-month internship by the academy to further develop my skills in Python",
+      "I started a Python course and, after completing it, was offered a 1-month internship by the academy to further develop my skills in Python.",
   },
 ];
