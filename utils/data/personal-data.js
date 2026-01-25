5de56c545ddb5448db4e545df7c6856b63c83a70
marigonemusliu <marigonamusliu48@gmail.com>
2026-01-25 21:53:07 +0100

    update

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -17,5 +17,5 @@ export const personalData = {
   leetcode: "https://leetcode.com/u/MarigonaMusliu/",
   devUsername: "marigonemusliu",
   resume:
-    "https://drive.google.com/file/d/1ghm1KT_uVQmrvGYnmF8QLgGRFlyg7WI4/view?usp=sharing",
+    "https://drive.google.com/file/d/1tU_PbCxZb-sHaun_yw2JRGqImGM3wqDI/view?usp=sharing",
 };
